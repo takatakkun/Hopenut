4e04f387269426a08d5a2dcf936cfc6d17bb22c0
takatakkun <tkttkm18@icloud.com>
2024-11-01 20:20:12 +0900

    paint押したときと離したとき

--- Hopenut/Assets/Script/PaintColor.cs
+++ Hopenut/Assets/Script/PaintColor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PaintColor : MonoBehaviour
@@ -10,28 +11,48 @@ public class PaintColor : MonoBehaviour
     public Material blue;
     public Material orange;
     public Material white;
+    private GameObject firstclickobj;
 
     void Update()
     {
-        // マウスクリックまたはタッチが行われたとき
         if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
         {
-            // カメラからクリック位置へのRayを生成
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            // RaycastHitオブジェクトでヒット情報を格納
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            if (Input.GetMouseButtonDown(0)) //スマホの時の処理がない
             {
-                // ヒットしたオブジェクトがクアッドであるかチェック
-                if (hit.collider.gameObject.CompareTag("Quad"))
+                // カメラからクリック位置へのRayを生成
+                Ray ray0 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                // RaycastHitオブジェクトでヒット情報を格納
+                if (Physics.Raycast(ray0, out RaycastHit hit0))
                 {
-                    // ヒットしたオブジェクトのRendererを取得してマテリアル変更
-                    Renderer quadRenderer = hit.collider.gameObject.GetComponent<Renderer>();
-                    quadRenderer.GetComponent<Renderer>().material.color = paintColor.color;
+                    firstclickobj = hit0.collider.gameObject;
                 }
             }
         }
+            // マウスクリックまたはタッチが行われたとき
+            if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+            {
+                if (Input.GetMouseButtonUp(0)) //スマホの時の処理がない
+                {
+                    // カメラからクリック位置へのRayを生成
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                    // RaycastHitオブジェクトでヒット情報を格納
+                    if (Physics.Raycast(ray, out RaycastHit hit))
+                    {
+                        // ヒットしたオブジェクトがクアッドであるかチェック
+                        if (hit.collider.gameObject.CompareTag("Quad") && firstclickobj == hit.collider.gameObject)
+                        {
+                            // ヒットしたオブジェクトのRendererを取得してマテリアル変更
+                            Renderer quadRenderer = hit.collider.gameObject.GetComponent<Renderer>();
+                            quadRenderer.GetComponent<Renderer>().material.color = paintColor.color;
+                        }
+                    }
+                }
+                firstclickobj = null;
+            }
     }
+
 
     public void RedTurn()
     {
