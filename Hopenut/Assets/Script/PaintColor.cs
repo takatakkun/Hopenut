0ba123da652e16584c23c423739128a167ad7fc3
takatakkun <tkttkm18@icloud.com>
2024-11-02 20:56:48 +0900

    親決めの途中

--- Hopenut/Assets/Script/PaintColor.cs
+++ Hopenut/Assets/Script/PaintColor.cs
@@ -11,8 +11,12 @@ public class PaintColor : MonoBehaviour
     public Material blue;
     public Material orange;
     public Material white;
-    private Vector2 firstclickobj;
-    private Vector2 lastclickobj;
+    private Vector2 firstclickpos;
+    private Vector2 lastclickpos;
+    private Vector3 objpos;
+    public bool rotationable;
+    private Vector2 clickingpos;
+    public FamilySet familyset;
 
     void Update()
     {
@@ -20,21 +24,26 @@ public class PaintColor : MonoBehaviour
         {
             Ray ray0;
             // カメラからクリック位置へのRayを生成
-            if (Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))                                     //マウス操作
             {
                 ray0 = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray0, out RaycastHit hit0))
                 {
-                    firstclickobj = Input.mousePosition;
+                    firstclickpos = Input.mousePosition;
+                    objpos = hit0.collider.transform.position;
+                    rotationable = false;
+                    Debug.Log(objpos);
                 }
             }
-            else
+            else                                                                 //スマホ操作
             {
                  ray0 = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
                 // RaycastHitオブジェクトでヒット情報を格納
                 if (Physics.Raycast(ray0, out RaycastHit hit0))
                 {
-                    firstclickobj = Input.GetTouch(0).position;
+                    firstclickpos = Input.GetTouch(0).position;
+                    objpos = hit0.collider.transform.position;
+                    rotationable = false;
                 }
             }
         }
@@ -43,29 +52,91 @@ public class PaintColor : MonoBehaviour
         {
             Ray ray;
             // カメラからクリック位置へのRayを生成
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))                                       //マウス操作
             {
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                lastclickobj = Input.mousePosition;
+                lastclickpos = Input.mousePosition;
+                rotationable = false;
+                familyset.GoodBye();
             }
-            else
+            else                                                                 //スマホ操作
             {
                 ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                lastclickobj = Input.GetTouch(0).position;
+                lastclickpos = Input.GetTouch(0).position;
+                rotationable = false;
+                familyset.GoodBye();
             }
 
             // RaycastHitオブジェクトでヒット情報を格納
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 // ヒットしたオブジェクトがクアッドであるかチェック
-                if (hit.collider.gameObject.CompareTag("Quad") && firstclickobj == lastclickobj)
+                if (hit.collider.gameObject.CompareTag("Quad") && firstclickpos == lastclickpos)
                 {
                     // ヒットしたオブジェクトのRendererを取得してマテリアル変更
                     Renderer quadRenderer = hit.collider.gameObject.GetComponent<Renderer>();
                     quadRenderer.GetComponent<Renderer>().material.color = paintColor.color;
                 }
             }
-                firstclickobj = lastclickobj;
+                firstclickpos = lastclickpos;
+        }
+
+        if ((Input.GetMouseButton(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary)) && rotationable == false)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                clickingpos = Input.mousePosition;
+                Vector2 dpos = clickingpos - firstclickpos;
+
+                if ((dpos.y > -dpos.x && dpos.y < dpos.x) || (dpos.y < -dpos.x && dpos.y > dpos.x))
+                {
+                    if (objpos.y > 1.7 && objpos.y < 2.3)
+                    {
+                        familyset.YPlusClick();
+                        rotationable = true;
+                    }
+                    if (objpos.y > -0.7 && objpos.y < 0.3)
+                    {
+                        familyset.YMinusClick();
+                        rotationable = true;
+                    }
+                }
+                if((dpos.y > dpos.x && dpos.y > -dpos.x) || (dpos.y < dpos.x && dpos.y < -dpos.x))
+                {
+                    if (objpos.x > 1.7 && objpos.x < 2.3)
+                    {
+                        familyset.XPlusClick();
+                        rotationable = true;
+                    }
+                    if (objpos.x > -0.7 && objpos.x < 0.3)
+                    {
+                        familyset.XMinusClick();
+                        rotationable = true;
+                    }
+                    if (objpos.z > 1.7 && objpos.z < 2.3)
+                    {
+                        familyset.ZPlusClick();
+                        rotationable = true;
+                    }
+                    if (objpos.z > -0.7 && objpos.z < 0.3)
+                    {
+                        familyset.ZMinusClick();
+                        rotationable = true;
+                    }
+                }
+            }
+            else
+            {
+                clickingpos = Input.GetTouch(0).position;
+                if (firstclickpos.x != clickingpos.x)
+                {
+
+                }
+                if (firstclickpos.y != clickingpos.y)
+                {
+
+                }
+            }
         }
     }
 
